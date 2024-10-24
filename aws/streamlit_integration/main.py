8a4f3af1dc08d64a2afb1f0ea3d6e5aefd994ebf
team-aws-admin <absnt.project@gmail.com>
2024-10-24 00:30:32 +0000

    added instructions and example of camera usage

--- aws/streamlit_integration/main.py
+++ aws/streamlit_integration/main.py
@@ -1,4 +1,14 @@
+
+# to use camera on http site, you must include the site on chrome settings, so go to <chrome://flags/#unsafely-treat-insecure-origin-as-secure> 
+#enable it and type the url befor using it.
+
 import streamlit as st
 
+st.title("Webcam Capture with Streamlit")
 
-st.write("hello world")
+# Display camera input
+picture = st.camera_input("Take a picture")
+
+# If a picture is taken, display it
+if picture:
+    st.image(picture)
